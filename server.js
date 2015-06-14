a9bc138ef4c5008a39f56b62bd5ebba4d66957cb
root <root@linux-y49p.site>
2015-06-15 01:19:30 +0530

    First Commit

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-var express=require('express');
-var env=process.env.NODE_ENV=process.env.NODE_ENV || 'development';
+var express = require('express');
 
-var app=express();
+stylus = require('stylus');
+logger = require('morgan');
+bodyParser = require('body-parser');
 
-app.set('views',__dirname + '/server/views');
-app.set('view engine','jade');
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-app.get('*',function(req,res)
+var app = express();
+
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(stylus.middleware(
     {
+        src: __dirname + '/public',
+        compile: compile
+    }
+));
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function (req, res) {
         res.render('index');
     }
 );
 
-var port=3030;
+var port = 3030;
 app.listen(port);
-console.log('Listening on port: '+port+'.....');
+console.log('Listening on port: ' + port + '.....');
